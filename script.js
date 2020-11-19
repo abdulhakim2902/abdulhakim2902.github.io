27239d0abfe2a65192d290e198fbb8ad4ee17e5c
abdulhakim2902 <abdulhakim2902@gmail.com>
2020-11-20 04:58:58 +0700

    Final Project

---
+++ script.js
@@ -0,0 +1,45 @@
+function randomPokemon() {
+    let randomNumber = Math.random()
+
+    if (randomNumber >= 0.66) return 'charmander';
+    return randomNumber >= 0.33 ? 'squirtle' : 'bulbasaur';
+}
+
+function result(player, computer) {
+    if (player == computer) return 0;
+    if (player == 'charmander') return computer == 'bulbasaur' ? 1 : -1;
+    if (player == 'squirtle') return computer == 'charmander' ? 1 : -1;
+    return computer == 'squirtle' ? 1 : -1;   
+}
+
+const countScore = {
+    player: 0,
+    computer: 0
+}
+
+document.querySelectorAll('.player-pokemon img').forEach(playerPokemon => {
+    playerPokemon.onclick = () => {
+        const pComputer = randomPokemon();
+        const score = result(playerPokemon.className, pComputer);
+        document.querySelector('.computer-img').src = `/img/${pComputer}.png`
+
+        if (score === 1) {
+            countScore.player += 3;
+            countScore.computer -= 1;
+        } else if (score === -1) {
+            countScore.player -= 1;
+            countScore.computer += 3;
+        }
+
+        document.querySelector('.player-score').innerHTML = countScore.player;
+        document.querySelector('.computer-score').innerHTML = countScore.computer;
+    }
+})
+
+document.querySelector('button').onclick = () => {
+    countScore.player = 0;
+    countScore.computer = 0;
+    document.querySelector('.player-score').innerHTML = countScore.player;
+    document.querySelector('.computer-score').innerHTML = countScore.computer;
+    document.querySelector('.computer-img').src = `/img/pokeball.png`
+}
